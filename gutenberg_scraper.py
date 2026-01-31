1997fb2d857569e516023d121263d66dac897dff
obito <yaboudra@gmail.com>
2026-01-31 22:15:37 +0100

    refactor: update file path for write operations

--- gutenberg_scraper.py
+++ gutenberg_scraper.py
@@ -1,12 +1,13 @@
 import requests
 import os
 
-os.makedirs("books", exist_ok=True)
+os.makedirs("../scraped_data/www.gutenberg.org", exist_ok=True)
 
 print("Requesting Webpage...")
 
-for i in range(1,77817):
+#77817
+for i in range(1,77817+1):
     r = requests.get("https://www.gutenberg.org/cache/epub/"+str(i)+"/pg"+str(i)+".txt")
     
-    with open("books/book"+str(i)+".txt", "w", encoding="utf-8") as f:
+    with open("../scraped_data/www.gutenberg.org/book"+str(i)+".txt", "w", encoding="utf-8") as f:
         f.write(r.text)
